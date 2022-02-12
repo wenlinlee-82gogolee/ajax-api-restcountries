c1f6a0c38f717d5e995b82e87be6db617aa6bdc2
Wen-Lin Lee <82gogolee@gmail.com>
2022-02-12 14:14:52 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+const button = document.querySelector('.button');
+const search = document.querySelector('.search-input');
+const countriesContainer = document.querySelector('.countries');
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `
+  <article class="country">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      data.population / 1000000
+    ).toFixed(1)} million</p>
+    <p class="country__row"><span>🗣️</span>${
+      Object.values(data.languages)[0]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      Object.keys(data.currencies)[0]
+    }</p>
+  </div>
+</article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+button.addEventListener('click', function () {
+  getCountryData(search.value.toLowerCase());
+});
